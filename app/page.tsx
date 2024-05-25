f5418bff373f579ef46bc16bd185500b962ff1e4
El-suraj <surajohussaini7@gmail.com>
2024-05-25 06:01:33 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 // import styles from '@/app/ui/home.module.css';
+import { lusitana } from './ui/fonts';
 import Image from "next/image";
-import { Lusitana } from 'next/font/google';
 
 export default function Page() {
   return (
